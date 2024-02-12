712d24036168a99bc1eef7be2a2fe4e719a3ec80
최원영 <eorkfclsrn@naver.com>
2024-02-13 01:39:54 +0900

    메인 로고 클릭시 쇼릴로 이동

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="text-gray-600 body-font border-b border-gray-300"> {/* 하단 선을 추가 */}
             <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
-                <Link legacyBehavior href="/projects">
+                <Link legacyBehavior href="/showreel">
                     <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
                         <Image src="/AURA_LOGO_02_1.png" alt="아우라로고2" width={200} height={200} priority={true} />
                     </a>
