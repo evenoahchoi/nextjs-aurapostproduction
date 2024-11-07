abcf413c630d3d20ad8530d406befa33d44e0268
evenoahchoi <eorkfclsrn@naver.com>
2024-11-08 00:55:52 +0900

    URL버전으로 수정

--- components/projects/project-item.js
+++ components/projects/project-item.js
@@ -1,3 +1,37 @@
+// 다운로드 버전
+// import Image from "next/legacy/image";
+// import Link from "next/link";
+
+// export default function ProjectItem({ data }) {
+//     const title = data.title || '';  // 제목
+//     const youtube = data.youtube || '';  // 유튜브 URL
+//     const description = data.description || '';  // 설명
+//     const projectId = data._id;  // MongoDB의 ObjectID를 사용
+//     const imagePath = `/images/${projectId}.jpg`; // public/images 폴더에서 _id를 파일명으로 사용
+
+//     return (
+//         <Link 
+//             legacyBehavior 
+//             href={`/projects/${encodeURIComponent(projectId)}?title=${encodeURIComponent(title)}&youtube=${encodeURIComponent(youtube)}&description=${encodeURIComponent(description)}`}
+//         >
+//             <a className="project-card">
+//                 <Image
+//                     className="rounded-xl"
+//                     src={imagePath}
+//                     alt="cover image"
+//                     width="80%"
+//                     height="50%"
+//                     layout="responsive"
+//                     objectFit="fill"
+//                     quality={100}
+//                 />
+//             </a>
+//         </Link>
+//     );
+// }
+
+
+// URL 버전
 import Image from "next/legacy/image";
 import Link from "next/link";
 
@@ -5,8 +39,8 @@ export default function ProjectItem({ data }) {
     const title = data.title || '';  // 제목
     const youtube = data.youtube || '';  // 유튜브 URL
     const description = data.description || '';  // 설명
-    const projectId = data._id;  // MongoDB의 ObjectID를 사용
-    const imagePath = `/images/${projectId}.jpg`; // public/images 폴더에서 _id를 파일명으로 사용
+    const imgSrc = data.imgSrc || '';  // 이미지 URL
+    const projectId = data.projectId;  // 프로젝트 ID
 
     return (
         <Link 
@@ -16,7 +50,7 @@ export default function ProjectItem({ data }) {
             <a className="project-card">
                 <Image
                     className="rounded-xl"
-                    src={imagePath}
+                    src={imgSrc}
                     alt="cover image"
                     width="80%"
                     height="50%"
